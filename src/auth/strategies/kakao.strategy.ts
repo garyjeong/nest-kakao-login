c8a997453ab8fdc2e10d09de3d26a7d0e5e78d90
rencarJJM <loters@imsmobility.co.kr>
2022-09-10 21:27:12 +0900

    🚧 카카오 로그인 구조

---
+++ src/auth/strategies/kakao.strategy.ts
@@ -0,0 +1,23 @@
+import { Strategy } from 'passport-kakao';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
+  constructor() {
+    super({
+      clientID: process.env.KAKAO_CLIENT_KEY,
+      callbackURL: process.env.KAKAO_CALLBACK_URL,
+    });
+  }
+
+  validate(accessToken: string, refreshToken: string, profile: any) {
+    console.log('validate');
+    return {
+      email: profile.emails[0].value,
+      password: '1111',
+      nickname: profile.displayName,
+      userImgURL: profile._json.profile_image,
+    };
+  }
+}
